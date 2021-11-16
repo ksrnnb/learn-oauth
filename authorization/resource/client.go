afee4cd6274f82e72ca016ff4dca819397d61082
ksrnnb <enrico.fermi.phys@gmail.com>
2021-11-17 07:33:50 +0900

    feat: リダイレクトURIの検証方法を修正 #8

--- authorization/resource/client.go
+++ authorization/resource/client.go
@@ -1,12 +1,15 @@
 package resource
 
-import "errors"
+import (
+	"errors"
+	"strings"
+)
 
 type Client struct {
 	Name         string
 	ClientId     string
 	ClientSecret string
-	RedirectUris  []string
+	RedirectUris []string
 }
 
 func clientsInMemory() []*Client {
@@ -15,17 +18,17 @@ func clientsInMemory() []*Client {
 			Name:         "ぴよぴよアプリ",
 			ClientId:     "abcde12345",
 			ClientSecret: "abcde12345secert",
-			RedirectUris:  []string{
+			RedirectUris: []string{
 				"http://localhost:3000/callback",
 				"http://localhost:3000/callback-no-state",
 			},
 		},
 		{
-			Name:         "dummy",
-			ClientId:     "dummy",
-			ClientSecret: "dummy",
-			RedirectUris:  []string{
-				"dummy",
+			Name:         "脆弱クライアント",
+			ClientId:     "xyz98765",
+			ClientSecret: "xyz98765secret",
+			RedirectUris: []string{
+				"http://localhost:3000",
 			},
 		},
 	}
@@ -46,9 +49,11 @@ func FindClient(clientId string) (*Client, error) {
 }
 
 // 引数のリダイレクトURIが設定されているかどうか
+// 完全一致である必要はない（ただし、完全一致がベター）
+// https://docs.github.com/ja/developers/apps/building-oauth-apps/authorizing-oauth-apps#redirect-urls
 func (c Client) HasRedirectUri(uri string) bool {
 	for _, u := range c.RedirectUris {
-		if u == uri {
+		if strings.HasPrefix(uri, u) {
 			return true
 		}
 	}
