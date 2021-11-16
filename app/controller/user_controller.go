f37465ea126b90fd4edd0fb51452016d70cceef5
ksrnnb <enrico.fermi.phys@gmail.com>
2021-11-17 08:24:48 +0900

    feat: Refererにクエリを含めるようヘッダーを設定 #8

--- app/controller/user_controller.go
+++ app/controller/user_controller.go
@@ -14,7 +14,9 @@ func NewUserController() UserController {
 
 // OAuth連携開始画面
 func (controller UserController) ShowUserPage(c echo.Context) error {
-	// TODO: これで合っているか確認
+	// https://developer.mozilla.org/ja/docs/Web/HTTP/Headers/Referrer-Policy
+	// Refererにクエリを含めるよう設定。通常は設定しない
+	c.Response().Header().Set("Referrer-Policy", "unsafe-url")
 
 	fileName := c.Param("name")
 	return c.Render(http.StatusOK, fileName, map[string]interface{}{
