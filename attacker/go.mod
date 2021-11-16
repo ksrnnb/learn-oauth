36a2a8a5a43e7f02598f7136813af5e01b6f070b
ksrnnb <enrico.fermi.phys@gmail.com>
2021-11-17 06:46:16 +0900

    feat: attackerのコンテナ作成

---
+++ attacker/go.mod
@@ -0,0 +1,5 @@
+module github.com/ksrnnb/learn-oauth/attacker
+
+go 1.16
+
+require github.com/labstack/echo/v4 v4.6.1 // indirect
---
+++ attacker/main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/labstack/echo/v4"
+)
+
+func main() {
+	e := echo.New()
+
+	e.GET("/", getCode)
+
+	e.Logger.Fatal(e.Start(":3000"))
+}
+
+func getCode(c echo.Context) error {
+	fmt.Println(c.Request().Referer())
+	return nil
+}
